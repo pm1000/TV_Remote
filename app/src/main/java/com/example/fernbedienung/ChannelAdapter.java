7c6b9cc78f3e80807235dd38faf5c04cb38650d2
Philipp <philipp.meier@stud.h-da.de>
2020-07-06 09:47:07 +0200

    before merge

--- app/src/main/java/com/example/fernbedienung/ChannelAdapter.java
+++ app/src/main/java/com/example/fernbedienung/ChannelAdapter.java
@@ -1,6 +1,7 @@
 package com.example.fernbedienung;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
